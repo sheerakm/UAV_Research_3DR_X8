3ab5defb20643d00190276ec02e51641bfe9864a
Sheerak Megerdichian <sheerak@Sheeraks-MacBook-Air.local>
2026-01-08 13:35:17 -0800

    Fixed a couple of bugs, now returns all 0s for observations, need to fix that

--- PPO_training.py
+++ PPO_training.py
@@ -12,16 +12,25 @@ from stable_baselines3.common.env_checker import check_env
 env = UAVEnv()
 check_env(env, warn=True)
 
-exit()
-
 
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=25000)
-model.save("UAV_Training")
+# model.save("UAV_Training")
 
 
-obs = env.reset()
+obs, info = env.reset()
+print("outside loop")
+print(obs)
 while True:
+    print("starting anew ")
+
     action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
+    obs, rewards, terminated, truncated, info = env.step(action)
+    print(obs)
+
+    if terminated or truncated :
+        obs = env.reset()
+        print("finished")
+        print("starting anew ")
+        print(obs)
+    # env.render()
